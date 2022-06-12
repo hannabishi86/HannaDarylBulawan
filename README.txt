1b8ca0c94fbbc812496ef7e4ce3e706549d3f133
hannabishi86 <bulawan.179565@globalcity.sti.edu.ph>
2022-06-12 19:27:12 +0800

    Portfolio

---
+++ README.txt
@@ -0,0 +1,5 @@
+Dimensions:
+
+For Desktop View: 1366 x 768
+For Tablet View: 786 x 768
+For Mobile View: 360 x 768
--- Script.js
+++ Script.js
@@ -15,30 +15,3 @@ if (navClose) {
   });
 }
 
-/***===== R E M O V E   M E N U   M O B I L E =====***/
-const navLinks = document.querySelectorAll(".nav-link");
-
-function linkAction() {
-  const navMenu = document.getElementById("nav-menu");
-  navMenu.classList.remove("show-menu");
-}
-navLinks.forEach((n) => n.addEventListener("click", linkAction));
-
-/***===== C H A N G E   B A C K G R O U N D   H E A D E R =====***/
-function scrollHeader() {
-  const header = document.getElementById("header");
-  if (this.scrollY >= 80) header.classList.add("scroll-header");
-  else header.classList.remove("scroll-header");
-}
-window.addEventListener("scroll", scrollHeader);
-
-/***===== T E S T I M O N I A L   S W I P E R =====***/
-var swiper = new Swiper(".testimonial-wrapper", {
-  spaceBetween: 30,
-  loop: "true",
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
